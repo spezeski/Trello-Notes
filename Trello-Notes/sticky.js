45e0419c7e237b817545fd42338a43baf66b4c7b
spezeski <james.spezeski@gmail.com>
2012-06-12 08:15:01 -0700

    Notes can now be filtered on, and hashtags ('#') are escaped when the notes are saved

--- Trello-Notes/sticky.js
+++ Trello-Notes/sticky.js
@@ -2,7 +2,10 @@ var id=location.href.replace(/.*\//,'');
 id=id.replace(/#/,'');
 code = 'var id=\''+id+'\';';
 
-code += 'function waitStickyPlugin(){var a=Models.Board.get(id);if(!a||!a.isReady||!a.listList.models||a.listList.models.length==0||$(".list-card").length==0){setTimeout(waitStickyPlugin,100)}else{initStickyPlugin()}}function show_sticky(a){var b=Models.Card.get(a);var c=b.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)?b.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1]:"";if(c=="")show_sticky_button(a);else show_sticky_text(a)}function show_sticky_button(a){var b=Models.Card.get(a);$(b.view.el).find(".badges").append($("<div>Note</div>").addClass("badge").click(function(){show_sticky_text(a)}).click(function(a){a.stopPropagation()}))}function show_sticky_text(a){var d=Models.Card.get(a);$(d.view.el).find(".card-labels").after($("<div></div>").append($("<input></input>").val(d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)?d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1]:"").addClass("sticy-note").attr("type","text").css("width","95%").css("height","20px").css("font-size","x-small").css("min-height","0px").css("background-color","lightyellow").css("margin","0px").css("padding-left","5px").blur(function(){d.api.update({idParents:[d.attributes.idList,d.attributes.idBoard],updates:[{set:{desc:">>>> "+$(this).val()+"\\n\\n"+d.attributes.desc.replace(/^>>>>[^\\n]*\\n\\n/,"")}}]},function(a){b.set(a,{fromServer:!0})},function(a){b?b(c,a):c.trigger("error",c,a,e)})})).click(function(a){a.stopPropagation()}).keydown(function(a){a.stopPropagation()}).keypress(function(a){a.stopPropagation()}).keyup(function(a){a.stopPropagation()}))}function modify_card(a){var b=Models.Card.get(a);if(!b.view){setTimeout(function(){modify_card(a)},100);return false}(function(){var a=b.view.render;b.view.render=function(){var c=a.apply(this,arguments);show_sticky(b.id);return c}})();(function(){var a=b.view.showDetail;b.view.showDetail=function(){openCard=$(arguments[0].delegateTarget).data().card.id;var b=a.apply(this,arguments);$(".js-close-window").click(function(){Models.Card.get(openCard).view.render()});return b}})();(function(){var a=b.ready;b.ready=function(){show_sticky(b.id)}})()}function initStickyPlugin(){$.each(Models.Board.get(id).listList.models,function(a,b){$.each(b.cardList.models,function(a,b){modify_card(b.id)});b.refreshCardList()})}waitStickyPlugin()';
+
+// This code is the minified version of the commented code below.
+// It needs to be minified and stored in a string so it can be injected into the page.
+code += 'function waitStickyPlugin(){var a=Models.Board.get(id);if(!a||!a.isReady||!a.listList.models||a.listList.models.length==0||$(".list-card").length==0){setTimeout(waitStickyPlugin,100)}else{initStickyPlugin()}}function show_sticky(a){var b=Models.Card.get(a);var c=b.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)?b.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1]:"";if(c=="")show_sticky_button(a);else show_sticky_text(a)}function show_sticky_button(a){var b=Models.Card.get(a);$(b.view.el).find(".badges").append($("<div>Note</div>").addClass("badge").click(function(){show_sticky_text(a)}).click(function(a){a.stopPropagation()}))}function show_sticky_text(a){var d=Models.Card.get(a);$(d.view.el).find("a.list-card-title").append($("<span></span>").css("display","none").text(d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)?d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1]:""));$(d.view.el).find(".card-labels").remove(".sticky-note").after($("<div></div>").append($("<input></input>").val((d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)?d.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1]:"").replace(/\\\\/,"")).addClass("sticky-note").attr("type","text").css("width","95%").css("height","20px").css("font-size","x-small").css("min-height","0px").css("background-color","lightyellow").css("margin","0px").css("padding-left","5px").blur(function(){d.api.update({idParents:[d.attributes.idList,d.attributes.idBoard],updates:[{set:{desc:">>>> "+$(this).val().replace(/#/,"\\\\#")+"\\n\\n"+d.attributes.desc.replace(/^>>>>[^\\n]*\\n\\n/,"")}}]},function(a){b.set(a,{fromServer:!0})},function(a){b?b(c,a):c.trigger("error",c,a,e)})})).click(function(a){a.stopPropagation()}).keydown(function(a){a.stopPropagation()}).keypress(function(a){a.stopPropagation()}).keyup(function(a){a.stopPropagation()}))}function modify_card(a){var b=Models.Card.get(a);if(!b.view){setTimeout(function(){modify_card(a)},100);return false}(function(){var a=b.view.render;b.view.render=function(){var c=a.apply(this,arguments);show_sticky(b.id);return c}})();(function(){var a=b.view.showDetail;b.view.showDetail=function(){openCard=$(arguments[0].delegateTarget).data().card.id;var b=a.apply(this,arguments);$(".js-close-window").click(function(){Models.Card.get(openCard).view.render()});return b}})();(function(){var a=b.ready;b.ready=function(){b.view.render()}})()}function initStickyPlugin(){$.each(Models.Board.get(id).listList.models,function(a,b){$.each(b.cardList.models,function(a,b){modify_card(b.id)});b.refreshCardList()})}waitStickyPlugin()';
 
 if(document.getElementById('stickyPlugin') == null){
 	var script=document.createElement('script');
@@ -16,12 +19,16 @@ if(document.getElementById('stickyPlugin') == null){
 }
 
 /********************************* Non-Minified Code ************************************************
-** This code gets minified so that it can be inserted into the page and run in Trello's environment.
+** This code gets minified so that it can be injected into the page and run in Trello's environment.
 ** I use http://jscompress.com/
-** The only issue is that all instances of \n need to be replaced with \\n
+** Some caveats are:
+**    all instances of \ need to be replaced with \\
+**    the code will go between single quotes, so single quotes should be avoided within the code
+**    if a single quote can't be avoided, it needs to be escaped after minification
 *****************************************************************************************************/
 
 /*
+// Wait for the models to be ready
 waitStickyPlugin();
 function waitStickyPlugin() {
 	var i = Models.Board.get(id);
@@ -31,7 +38,6 @@ function waitStickyPlugin() {
 		initStickyPlugin();
 	}
 }
-
 
 // If there is a note, show it.  If not, show a button to add a note.
 function show_sticky(card_id) {
@@ -55,12 +61,26 @@ function show_sticky_button(card_id) {
 
 function show_sticky_text(card_id) {
 	var card=Models.Card.get(card_id);
-	$(card.view.el).find(".card-labels").after( $("<div></div>")
+	
+	// First put the text of the note inside the <a> tag so it can be used for filtering
+	$(card.view.el).find("a.list-card-title")
+	.append( $("<span></span>")
+		.css("display","none")
+		.text(
+			(card.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/) ? card.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1] : "")
+		)
+	);
+
+	// Then insert the text box for editing the note
+	$(card.view.el).find(".card-labels")
+	.remove(".sticky-note")
+	.after( $("<div></div>")
 		.append( $("<input></input>")
 			.val(
 				(card.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/) ? card.attributes.desc.match(/>>>> ([^\\n]*)\\n\\n/)[1] : "")
+				.replace(/\\\\/,"")
 			)
-			.addClass("sticy-note")
+			.addClass("sticky-note")
 			.attr("type","text")
 			.css("width","95%")
 			.css("height","20px")
@@ -75,7 +95,7 @@ function show_sticky_text(card_id) {
 						idParents:[card.attributes.idList,card.attributes.idBoard],
 						updates:[{set:{
 							desc:">>>> "
-							+$(this).val()
+							+$(this).val().replace(/#/,'\\\\#')
 							+"\\n\\n"+card.attributes.desc.replace(/^>>>>[^\\n]*\\n\\n/,"")
 						}}]
 					},
@@ -91,6 +111,7 @@ function show_sticky_text(card_id) {
 	)
 }
 
+// Override the Trello functions that would normally cause the notes to disappear.
 function modify_card(card_id) {
 	var card = Models.Card.get(card_id);
 	
@@ -124,7 +145,7 @@ function modify_card(card_id) {
 	})();
 		
 	// When cards are changed on the client side, they revert back to how they were before the
-	// change while the send the data to the server.  The server then sends the changes back
+	// change while they send the data to the server.  The server then sends the changes back
 	// and they are made again in the client.  During the time that this is happening, the notes disappear.
 	// The responsible function is card.ready()  Overriding it allows us to show the notes immediately
 	// after the card "reverts."  Disabling the "revert" functionality completely allows the notes
@@ -133,7 +154,7 @@ function modify_card(card_id) {
 		var proxied = card.ready
 		card.ready = function() {
 			//var v = proxied.apply(this, arguments);
-			show_sticky(card.id);
+			card.view.render();
 			//return v;
 		 };
 	})();
